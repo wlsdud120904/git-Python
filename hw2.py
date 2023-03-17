1f9e1a1f7ef2131b459ecdf775e4ab52f227c9ae
wlsdud120904 <1209wlsdud@naver.com>
2023-03-17 15:36:23 +0900

    hw2 upload

---
+++ hw2.py
@@ -0,0 +1,20 @@
+def exchange(money):
+    n500 = money // 500
+    money %= 500
+    n100 = money // 100
+    money %= 100
+    n50 = money // 50
+    money %= 50
+    n10= money // 10
+    money %= 10
+    print('500원 동전의 개수: ', n500)
+    print('100원 동전의 개수: ', n100)
+    print('50원 동전의 개수: ', n50)
+    print('10원 동전의 개수: ', n10)
+    
+def get_integer(prompt):
+    n = int(input(prompt))
+    return n
+
+money = get_integer('동전으로 교환하고자 하는 금액은? ')
+exchange(money)
